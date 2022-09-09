5a2ee6be93d7ca9fbf93811d74f260d2e13e75de
Bradley-Laubscher <bradleylaubscher@gmail.com>
2022-09-09 10:55:56 +0200

    used next.js Image component for optimization

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'cdn.kimkim.com',
+      'www.thespiritsbusiness.com',
+      'www.traveldudes.com',
+      'www.tripsavvy.com',
+      'lp-cms-production.imgix.net',
+      'cdn.kiwicollection.com',
+      'p-airnz.com',
+      'lh3.googleusercontent.com',
+      'media.tacdn.com',
+      'specials-images.forbesimg.com',
+      'resources'      
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/header';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -15,44 +16,124 @@ export default function Home() {
       <main>
         <h2 className="page-heading destinations">Top Travel Destinations</h2>
         <section className='gallery-container'>
-          <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/South_Island" target="blank"><img src="https://cdn.kimkim.com/files/a/content_articles/featured_photos/3117192df163b1c655a6081e58961533bd8e1afe/medium-3dcab0806698d21029384a2602890a74.jpg" alt="icy green mountains and clear lake" /></a>
+        <fig className="gallery">
+            <a href="https://en.wikipedia.org/wiki/South_Island" target="blank">
+              <Image
+               src="https://cdn.kimkim.com/files/a/content_articles/featured_photos/3117192df163b1c655a6081e58961533bd8e1afe/medium-3dcab0806698d21029384a2602890a74.jpg" 
+               alt="icy green mountains and clear lake" 
+               width='300'
+               height='271'
+               layout='responsive'
+              />
+            </a>
             <figcaption className="figcaption">South Islands, New Zealand</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Paris" target="blank"><img src="https://www.thespiritsbusiness.com/content/http://www.thespiritsbusiness.com/media/2020/07/Paris.jpg" alt="The Eiffel Tower" /></a>
+            <a href="https://en.wikipedia.org/wiki/Paris" target="blank">
+              <Image 
+                src="https://www.thespiritsbusiness.com/content/http://www.thespiritsbusiness.com/media/2020/07/Paris.jpg" 
+                alt="The Eiffel Tower"
+                width='300'
+                height='271'
+                layout='responsive' 
+              />
+            </a>
             <figcaption className="figcaption">Paris</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Bora_Bora" target="blank"><img src="https://www.traveldudes.com/wp-content/uploads/2020/09/beach-bora-bora-clouds-753626.jpg" alt="cozy huts on the ocean" /></a>
+            <a href="https://en.wikipedia.org/wiki/Bora_Bora" target="blank">
+              <Image 
+                src="https://www.traveldudes.com/wp-content/uploads/2020/09/beach-bora-bora-clouds-753626.jpg" 
+                alt="cozy huts on the ocean" 
+                width='300'
+                height='271'
+                layout='responsive' 
+              />
+              </a>
             <figcaption className="figcaption">Bora Bora</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Glacier_National_Park_(U.S.)" target="blank"><img src="https://www.tripsavvy.com/thmb/vg2Q9JbZCYCmfBkqLh_GCi4n7C0=/2700x1800/filters:fill(auto,1)/IMG_7115-e21fc727cf734a2fb38de800930605b3.jpg" alt="beautiful mountains with a glacial lake bvelow" /></a>
+            <a href="https://en.wikipedia.org/wiki/Glacier_National_Park_(U.S.)" target="blank">
+              <Image 
+                src="https://www.tripsavvy.com/thmb/vg2Q9JbZCYCmfBkqLh_GCi4n7C0=/2700x1800/filters:fill(auto,1)/IMG_7115-e21fc727cf734a2fb38de800930605b3.jpg" 
+                alt="beautiful mountains with a glacial lake bvelow" 
+                width='300'
+                height='271'
+                layout='responsive'
+              />
+            </a>
             <figcaption className="figcaption">Glacier National Park</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/London" target="blank"><img src="https://lp-cms-production.imgix.net/2019-06/55425108.jpg" alt="Big Ben" /></a>
+            <a href="https://en.wikipedia.org/wiki/London" target="blank">
+              <Image 
+                src="https://lp-cms-production.imgix.net/2019-06/55425108.jpg" 
+                alt="Big Ben" 
+                width='300'
+                height='271'
+                layout='responsive'
+              />
+            </a>
             <figcaption className="figcaption">London</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Maui" target="blank"><img src="https://cdn.kiwicollection.com/media/property/PR010193/xl/010193-02-Hyatt-Regency-Maui-Japengo-from-Halona-Kai-Pink-Small.jpg?cb=1597093511" alt="Palrm tree resort" /></a>
+            <a href="https://en.wikipedia.org/wiki/Maui" target="blank">
+              <Image 
+                src="https://cdn.kiwicollection.com/media/property/PR010193/xl/010193-02-Hyatt-Regency-Maui-Japengo-from-Halona-Kai-Pink-Small.jpg?cb=1597093511" 
+                alt="Palrm tree resort" 
+                width='300'
+                height='271'
+                layout='responsive'
+              />
+            </a>
             <figcaption className="figcaption">Maui</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Tahiti" target="blank"><img src="https://p-airnz.com/cms/assets/Common-Assets/Destination-Images/pacific-islands-tahiti-huts-lagoon-fish-1200x800__ScaleMaxWidthWzczOF0.jpg" alr="huts over the ocean" /></a>
+            <a href="https://en.wikipedia.org/wiki/Tahiti" target="blank">
+              <Image 
+                src="https://p-airnz.com/cms/assets/Common-Assets/Destination-Images/pacific-islands-tahiti-huts-lagoon-fish-1200x800__ScaleMaxWidthWzczOF0.jpg" 
+                alr="huts over the ocean" 
+                width='300'
+                height='271'
+                layout='responsive'
+              />
+            </a>
             <figcaption className="figcaption">Tahiti</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Tokyo" target="blank"><img src="https://lh3.googleusercontent.com/7wFv_iLVUp7Y1S017TamjvsQJdCbVkDeTPwvLiNh3H7MO5yxror8FOQcu8b2tLd-dsy6EheyUbcd_1XQdToM=s750" alt="Tokyo city at night" /></a>
+            <a href="https://en.wikipedia.org/wiki/Tokyo" target="blank">
+              <Image 
+                src="https://lh3.googleusercontent.com/7wFv_iLVUp7Y1S017TamjvsQJdCbVkDeTPwvLiNh3H7MO5yxror8FOQcu8b2tLd-dsy6EheyUbcd_1XQdToM=s750" 
+                alt="Tokyo city at night"
+                width='300'
+                height='271'
+                layout='responsive' 
+              />
+            </a>
             <figcaption className="figcaption">Tokyo</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Rome" target="blank"><img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6c/2c/70.jpg" alt="the Colloseum" /></a>
+            <a href="https://en.wikipedia.org/wiki/Rome" target="blank">
+              <Image 
+                src="https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6c/2c/70.jpg" 
+                alt="the Colloseum" 
+                width='300'
+                height='271'
+                layout='responsive'
+              />
+            </a>
             <figcaption className="figcaption">Rome</figcaption>
           </fig>
           <fig className="gallery">
-            <a href="https://en.wikipedia.org/wiki/Phuket_Province" target="blank"><img src="https://specials-images.forbesimg.com/imageserve/1071599218/960x0.jpg?fit=scale" alt="Ocean Mountains" /></a>
+            <a href="https://en.wikipedia.org/wiki/Phuket_Province" target="blank">
+              <Image 
+                src="https://specials-images.forbesimg.com/imageserve/1071599218/960x0.jpg?fit=scale" 
+                alt="Ocean Mountains"
+                width='300'
+                height='271'
+                layout='responsive' 
+              />
+            </a>
             <figcaption className="figcaption">Phuket</figcaption>
           </fig>
         </section>
